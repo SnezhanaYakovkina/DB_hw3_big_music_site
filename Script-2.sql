19b3f2d550b6d8860a0770e9502c0ce3c19f4d35
Snezhana Yakovkina <snezhanayakovkina@gmail.com>
2021-11-29 12:01:03 +0900

    First changes

--- Script-2.sql
+++ Script-2.sql
@@ -6,11 +6,18 @@ track_id integer not null,
 album_id integer not null
 );
 
+alter table collections drop column track_id;
+alter table collections drop column album_id;
+
+
 create table if not exists genres(
 genre_id serial primary key,
 genre_name varchar(40) not null,
 singer_id integer not null
 );
+
+alter table genres drop column singer_id;
+
 
 create table if not exists albums(
 album_id serial primary key,
@@ -19,11 +26,17 @@ album_title varchar(100) not null,
 album_year integer not null
 );
 
+alter table albums drop column singer_id;
+
+
 create table if not exists singers(
 singer_id serial primary key,
 genre_id integer not null,
 singer_name varchar(100) not null
 );
+
+alter table singers drop column genre_id;
+
 
 create table if not exists tracks(
 track_id serial primary key,
@@ -32,17 +45,20 @@ track_duration integer not null,
 album_id integer references albums(album_id)
 );
 
+
 create table if not exists collection_tracks(
 collection_tracks_id serial primary key,
 track_id integer not null references tracks(track_id),
 collection_id integer not null references collections(collection_id)
 );
 
+
 create table if not exists collaborations(
 collaboration_id serial primary key,
 singer_id integer not null references singers(singer_id),
 album_id integer not null references albums(album_id)
 );
+
 
 create table if not exists multigenre(
 multigenre_id serial primary key,
